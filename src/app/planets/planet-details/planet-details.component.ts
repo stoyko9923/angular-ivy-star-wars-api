2c7303dc102a357e91c5e4edacb10bf2f5e1033e
stoyko9923 <118949216+stoyko9923@users.noreply.github.com>
2022-11-27 21:46:19 +0000

    1 file modified

--- src/app/planets/planet-details/planet-details.component.ts
+++ src/app/planets/planet-details/planet-details.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planet-details.component.css'],
 })
 export class PlanetDetailsComponent implements OnInit {
+  @Input() planet: Planet;
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getPlanet();
+  }
+
+  getPlanet() {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.api.getPlanet(id).subscribe((planet) => (this.planet = planet));
+  }
 }
